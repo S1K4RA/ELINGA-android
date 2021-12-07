a51c2fae5547718081db8dbc3ed69cf5e9d79ddf
S1K4RA <73513381+S1K4RA@users.noreply.github.com>
2021-12-07 22:03:51 +0700

    Yay firebase connect done

--- app/src/main/java/com/kelompok9/elinga/MainActivity.kt
+++ app/src/main/java/com/kelompok9/elinga/MainActivity.kt
@@ -6,13 +6,25 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 lateinit var db : FirebaseFirestore
 
+
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         db = FirebaseFirestore.getInstance()
 
+        val data = "Test1"
+
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Ada"
+        user["last"] = "Lovelace"
+        user["born"] = 1815
+
+        db.collection("TestingDB").document().set(user)
         //hello
     }
 }
