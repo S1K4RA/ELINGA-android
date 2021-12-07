8dc4d78c9c802ffd1c9790933d769067238aeeb4
Anderson-bangke <75999604+Anderson-bangke@users.noreply.github.com>
2021-12-07 21:36:25 +0700

    Connect ke Firebase

--- app/src/main/java/com/kelompok9/elinga/MainActivity.kt
+++ app/src/main/java/com/kelompok9/elinga/MainActivity.kt
@@ -2,11 +2,16 @@ package com.kelompok9.elinga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+
+lateinit var db : FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = FirebaseFirestore.getInstance()
 
         //hello
     }
