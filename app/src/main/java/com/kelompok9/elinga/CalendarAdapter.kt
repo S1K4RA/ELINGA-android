4b053b52c3391a120e4a6281eb221956f488044e
S1K4RA <73513381+S1K4RA@users.noreply.github.com>
2021-12-29 15:39:57 +0700

    fix calendar

--- app/src/main/java/com/kelompok9/elinga/CalendarAdapter.kt
+++ app/src/main/java/com/kelompok9/elinga/CalendarAdapter.kt
@@ -14,7 +14,7 @@ class CalendarAdapter(
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.calendar_cell, parent, false)
         val layoutParams = view.layoutParams
-        layoutParams.height = (parent.height * 0.166666666).toInt()
+        layoutParams.height = (parent.height * 0.14).toInt()
         return CalendarViewHolder(view, onItemListener)
     }
 
